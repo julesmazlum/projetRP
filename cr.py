46cfb4e91ace26f39b80667dc7f1aa4ef603e020
julesmazlum <julesm.etudiant@gmail.com>
2025-04-27 19:41:42 +0200

    correction affichage

--- cr.py
+++ cr.py
@@ -184,8 +184,8 @@ def chercher_alternative(tour, current, sens, graphe, non_visites, start_index, 
 
     # Données
     n = len(tour)
-    log(f"Recherche {f"interne d'alternative depuis {current} vers {next_sommet_original}" if not skip_visited else f"externe d'alternative depuis {current}"}")
-
+    texte = f"interne d'alternative depuis {current} vers {next_sommet_original}" if not skip_visited else f"externe d'alternative depuis {current}"
+    log(f"Recherche {texte}")
 
     for j in range(2, n):
         # Index sommet tentatif
